8cad9b2679c3e8ed63d58de355be5875d2ba6635
ai357 <112920373+ai357@users.noreply.github.com>
2023-06-04 17:16:18 +0900

    チャプター2

--- Enter/src/paiza.java
+++ Enter/src/paiza.java
@@ -1,5 +1,7 @@
 public class paiza {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello paiza");
+        System.out.println("世界の皆さん、こんにちは");
     }
 }
