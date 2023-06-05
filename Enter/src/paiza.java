3a6332804d7064b77b49f9142c71fdd2dc68253e
ai357 <112920373+ai357@users.noreply.github.com>
2023-06-05 22:26:46 +0900

    chapter3-2

--- Enter/src/paiza.java
+++ Enter/src/paiza.java
@@ -1,6 +1,11 @@
+// Java練習
+/*
+ * 行をまたいでコメント
+ */
+
 public class paiza {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world"); //同じ行にもコメントできる
         System.out.println("Hello paiza");
         System.out.println("世界の皆さん、こんにちは");
         System.out.println("hello paiza");
