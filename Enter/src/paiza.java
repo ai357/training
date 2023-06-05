e3d652f5770b84e526ca3d660a59a8123558615f
ai357 <112920373+ai357@users.noreply.github.com>
2023-06-05 21:57:03 +0900

    chapter3-1

--- Enter/src/paiza.java
+++ Enter/src/paiza.java
@@ -3,5 +3,7 @@ public class paiza {
         System.out.println("Hello world");
         System.out.println("Hello paiza");
         System.out.println("世界の皆さん、こんにちは");
+        System.out.println("hello paiza");
     }
 }
+
