5aad8f63a7a4ca6f94c9f025ff40ae91f267205b
ai357 <112920373+ai357@users.noreply.github.com>
2023-06-26 21:32:13 +0900

    ALH

--- Enter/src/ALH.java
+++ Enter/src/ALH.java
@@ -9,9 +9,19 @@ public class ALH {
         String strFizz ="Fizz";
         String strBuzz ="Buzz";
 
-        
+        for ( i = 1; i <= loopCount; i++ ){
+            if ( i % (numFizz * numBuzz) == 0 ){
+                System.out.println(strFizz + strBuzz);
+            } else if ( i % numFizz == 0 ){
+                System.out.println(strFizz);   
+            } else if ( i % numBuzz == 0){
+                System.out.println(strBuzz);
+            } else {
+                System.out.println(i);
+            }
+        }
     }
     
     }
     
-}
+
