b4bbaedd7756471cf819b4d7cd04793b6b6a9208
ai357 <112920373+ai357@users.noreply.github.com>
2023-07-07 23:37:18 +0900

    chapter3-1

--- Enter/src/progate.java
+++ Enter/src/progate.java
@@ -11,12 +11,14 @@ public class progate {
             System.out.println("吉です");
             break; 
         }
-    
-    
-    
-    
-    
-    
       }
     
+    public static void main(String[] args) {
+      printData("Kete Jones");
+      printDate("Jhon Cristopher Smith");
+    }
+    
+    public static void printDate(String name){
+      System.out.println("私の名前は"+name+"です");
+    }
 }
