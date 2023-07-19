8f1ae78488fc2ecbde33e0dbd462d7db9ad9a241
ai357 <112920373+ai357@users.noreply.github.com>
2023-07-19 12:24:55 +0900

    game

---
+++ Enter/src/game/main.java
@@ -0,0 +1,14 @@
+class game {
+    
+    public static void main (String[] args) {
+
+        printData("Kate",27);
+        printData("Jhon",30);
+    }
+
+    public static void printData (String name, int age) {
+
+        System.out.println("私の名前は" + name + "です");
+        System.out.println("私の年齢は" + age + "です");
+    }
+}
